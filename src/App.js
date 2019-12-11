89be234a9f4cd5b2f83f799b673a13efa6ec4f83
Chris Oakes <chris.oakes@outlook.com>
2019-12-10 18:46:47 -0800

    chris-oakes

--- src/App.js
+++ src/App.js
@@ -13,16 +13,11 @@ import Display from "./components/DisplayComponents/Display"
 import Logo from "./components/DisplayComponents/Logo";
 
 function App() {
-  // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
-  // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
-  // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
-  // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
-  // Don't forget to pass the functions (and any additional data needed) to the components as props
-
 
   const [displayValue, setDisplayValue] = useState('0')
-  const addNumber = (number) => {
-    setDisplayValue(displayValue => displayValue + number)
+  console.log(setDisplayValue)
+  const addNumber = (num) => {
+    setDisplayValue(displayValue => displayValue + num)
   }
 
   return (
